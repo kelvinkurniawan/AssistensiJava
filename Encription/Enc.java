5a70c775130319ba7d795e654e6886e2296f65ea
Kelvin Kurniawan Oktavianto <37462966+kelvinkurniawan@users.noreply.github.com>
2020-03-18 17:17:01 +0700

    Update Enc.java

--- Encription/Enc.java
+++ Encription/Enc.java
@@ -1,13 +1,13 @@
 public class Enc {
     
     public static void main(String[] args) {
-        System.out.println(encrypt("Kelvin kurniawan", 9));
+        System.out.println(encrypt("Kelvin", 10)); // Memanggil fungsi encript
     }
     
-    public static String encrypt(String data, int key) { // Membuat fungsi untuk enkripsi
-        String ret = "";
-        for (char c : data.toCharArray()) {
-            if(c >= 'a' && c <= 'z') {
+    public static String encrypt(String data, int key) { // Membuat fungsi untuk enkripsi, parameter pertama adalah kata/kalimat, parameter kedua adalah key
+        String ret = ""; // Membuat variable ret untuk menghasilkan string dari hasil encrypt
+        for (char c : data.toCharArray()) { // Melakukan perulangan dan mengambil setiap karakter dari string yang ada diparameter pertama dan menampungnya di variable c
+            if(c >= 'a' && c <= 'z') { // Memeriksa jika karakter berada diantara a - z
                 c = (char) (c + key);
                 if(c > 'z') {
                     c = (char) (c+'a'-'z'-1);
